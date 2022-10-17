3083e5eb289204f2bf95324e748c3362fab7df15
Muchiru <81693306+kurialewis@users.noreply.github.com>
2022-10-17 14:46:37 +0300

    adminprofile&passwordreset

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin;
+// use Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AdminProfileController extends Controller
 {
@@ -12,5 +15,62 @@ class AdminProfileController extends Controller
         $adminData = Admin::find(1);
         // return view('admin.admin_profile_view', compact('adminData'));
         return view('admin\admin_profle_view', compact('adminData'));
+    }
+
+    public function AdminProfileEdit(){
+
+        $editData = Admin::find(1);
+        return view('admin\admin_profile_edit', compact('editData'));
+
+    }
+
+    public function AdminProfileStore(Request $request){
+        $data = Admin::find(1);
+        $data ->name = $request->name;
+        $data ->email = $request->email;
+
+        // code to upload photo
+        if($request->file('profile_photo_path')){
+            $file = $request->file('profile_photo_path');
+            // Replaces and deletes the previous image 
+            @unlink(public_path('upload/admin_images/' .$data->profile_photo_path)); 
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['profile_photo_path'] =  $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' =>'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+
+        );
+        return redirect()->route('admin.profile');
+
+    }
+
+    public function AdminChangePassword(){
+        return view('admin.admin_change_password');
+    }
+
+    public function AdminUpdateChangePassword(Request $request){
+
+      $validateData = $request->validate([
+        'oldpassword' => 'required',
+        'password' =>'required|confirmed',
+
+      ]);
+
+      $hashedPassword = Admin::find(1)->password;
+      if (Hash::check($request->oldpassword,$hashedPassword)) {
+        $admin = Admin::find(1);
+        $admin->password = Hash::make($request->password);
+        $admin->save();
+        Auth::logout();
+        return redirect()->route('admin.logout');
+      }else{
+        return redirect()->back();
+      }
+
     }
 }
